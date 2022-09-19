afdc8a6b8ff24d2fcad7305539a5cb12e6a0d0ba
alex <alex@pop-os.localdomain>
2022-09-19 21:12:26 +0200

    Dodata mala  skripta za proveru porta

--- network_port.sh
+++ network_port.sh
@@ -5,3 +5,4 @@ echo "Skripta nam prikazuje koji su otvoreni portovi za IPv4 na serveru "
 read -p "Upisi -4 za ipv4 -6 za ipv6: " IPV
 netstat -tunl "${IPV}" | grep  ':' | awk '{print $4}' | awk -F ':' '{print $NF}'|sort
 # $NF znaci poslednje polje
+# Moze se prosiriti sa nekim proverama ako treba
